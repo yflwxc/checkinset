e2a7c7e7d9bdf96676a780af84f77234ed3cebea
XCWang <90515143+yflwxc@users.noreply.github.com>
2022-05-22 20:39:11 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -7,7 +7,7 @@ headers = {
     'accept-language': 'zh-CN,zh;q=0.9',
     'content-length': '26',
     'content-type': 'application/json;charset=UTF-8',
-    'cookie': '_ga=GA1.2.2050194521.1639477580; koa:sess=eyJ1c2VySWQiOjExNzY5NCwiX2V4cGlyZSI6MTY2NTM5NzgyMDMwMSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=D_iTbGW7dJO3ToaVN9ndXbfOets; _gid=GA1.2.206998597.1644655233',
+    'cookie': '_ga=GA1.2.1504803283.1653209821; _gid=GA1.2.1931333058.1653209821; koa:sess=eyJ1c2VySWQiOjE2MjQ4NCwiX2V4cGlyZSI6MTY3OTEzNDIxNjYzNCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=cUEQZ5TpaQrBcAryT2vrR3enL1o; __stripe_mid=3d5e7b7e-b163-452a-bb9c-e27d59b32a8ccf5bb7; __stripe_sid=707afb32-127a-4f3f-85aa-e5280fa849921e1f4b',
     'origin': 'https://glados.rocks',
     'referer': 'https://glados.rocks/console/checkin',
     'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
